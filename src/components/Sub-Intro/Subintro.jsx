8bb7e2bbb3a04f4c5147e3936242d1bae78bfea0
unknown <nafizahmed698@gmail.com>
2025-07-01 21:50:35 +0600

    new commit

---
+++ src/components/Sub-Intro/Subintro.jsx
@@ -0,0 +1,62 @@
+import './Subintro.css';
+
+const Subintro = ({ heading, description, buttonText, IsHead , subHeadName , IsButton , onClick }) => {
+  return (
+    <div className="subintro-container">
+      {
+        IsHead == false &&
+        <div className="row subintro-name-row">
+        <div className="col text-center" style={{display: 'flex', justifyContent: 'center'}}>
+          <div className="subintro-name-div">
+            <p>{subHeadName}</p>
+          </div>
+        </div>
+      </div>
+      }
+      <div className="subintro-head-row">
+        <div className="text-center">
+          <p className={`${IsHead ? 'subintro-heading1' : 'subintro-heading2'}`}>{heading}</p>
+        </div>
+      </div>
+      <div className="subintro-description-row">
+        <div className="text-center">
+          <p className="subintro-description">{description}</p>
+        </div>
+      </div>
+      {
+      IsButton == true &&
+        <div className="row subintro-btn-row">
+        <div className="col subintro-btn-col">
+          <button className="subintro-btn" onClick={onClick}>
+            {buttonText}
+            <div className="subintro-icon">
+              <svg
+                width="21"
+                height="18"
+                viewBox="0 0 21 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.7803 1.15356L20.0002 8.99983L11.7803 16.8461"
+                  strokeWidth="1.28571"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M19 8.99976L1 8.99976"
+                  strokeWidth="1.28571"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+          </button>
+        </div>
+      </div>
+      }
+    </div>
+  );
+};
+
+export default Subintro;
